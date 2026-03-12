323713ed7b41eac57760b57eaeb70a422e6d4c8a
Diego Combariza <diego.combariza@uptc.edu.co>
2026-03-12 18:31:47 -0500

    wiii

--- drivers/connect-db.mjs
+++ drivers/connect-db.mjs
@@ -6,7 +6,7 @@ dotenv.config();
 async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log('MongoDB conectado:', mongoose.connection.host);
+        console.log('MongoDB conectado:', process.env.MONGODB_URI);
     } catch (error) {
         console.error('Error al conectar con MongoDB:', error);
         process.exit(1);
